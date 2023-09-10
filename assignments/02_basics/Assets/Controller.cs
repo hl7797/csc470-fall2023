b1386f20c8ab1188aa3d1856204c236cbe80edd7
hl7797 <li20010829@gmail.com>
2023-09-10 15:52:22 -0400

    fix

--- assignments/02_basics/Assets/Controller.cs
+++ assignments/02_basics/Assets/Controller.cs
@@ -25,33 +25,33 @@ public Camera cam;
         
        if (Input.GetKey(KeyCode.W)|Input.GetKey(KeyCode.UpArrow))
         {
-            //transform.Translate(Vector3.forward*m_speed*Time.deltaTime);
-            Vector3 movement = new Vector3(0, 0, 5); // 将输入值转换成三维向量
+            
+            Vector3 movement = new Vector3(0, 0, 5); 
         rigi.AddForce(movement * m_speed);
 
         }
         if (Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.DownArrow))
         {
-           // transform.Translate(Vector3.forward *- m_speed * Time.deltaTime);
-            Vector3 movement = new Vector3(0, 0, 5); // 将输入值转换成三维向量
+           
+            Vector3 movement = new Vector3(0, 0, 5); 
         rigi.AddForce(movement * -m_speed);
         }
         if (Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow))
         {
-            //transform.Translate(Vector3.right *-m_speed * Time.deltaTime);
-            Vector3 movement = new Vector3(5, 0, 0); // 将输入值转换成三维向量
+            
+            Vector3 movement = new Vector3(5, 0, 0); 
         rigi.AddForce(movement * -m_speed);
         }
         if (Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow))
         {
-           // transform.Translate(Vector3.right * m_speed * Time.deltaTime);
-            Vector3 movement = new Vector3(5, 0, 0); // 将输入值转换成三维向量
+          
+            Vector3 movement = new Vector3(5, 0, 0);
         rigi.AddForce(movement * m_speed);
         }
          if (Input.GetKey(KeyCode.Space))
         {
-           // transform.Translate(Vector3.right * m_speed * Time.deltaTime);
-            Vector3 movement = new Vector3(0, 5, 0); // 将输入值转换成三维向量
+          
+            Vector3 movement = new Vector3(0, 5, 0);
         rigi.AddForce(movement * m_speed);
         }
         cam.transform.position +=transform.position-offset;
