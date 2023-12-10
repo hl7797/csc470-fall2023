5afe2d9501b43a8c57b490bd404b96de2410048b
hl7797 <li20010829@gmail.com>
2023-12-10 08:07:31 -0500

    1

--- assignments/final/Assets/Script/MouseComponent.cs
+++ assignments/final/Assets/Script/MouseComponent.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class MouseComponent : MonoBehaviour
 {
     public SFader sceneFader;
-    public float mouseSensitivity = 100f;
-    public Transform playerBody;
-    float xRotation = 0f;
-
+    
    
     void Start()
     {
@@ -19,13 +16,7 @@ public class MouseComponent : MonoBehaviour
    
     void LateUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
-
+      
         if(Input.GetKey(KeyCode.E))
         {
             sceneFader.FadeOut("SampleScene");
